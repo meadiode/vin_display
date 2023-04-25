d0280fc413578bf5ea5cbbdbcb083eb7904795f4
Michael P <meadiode@protonmail.com>
2023-04-25 21:59:07 +0200

    mdl2416c_x10 pcb fixes/adjustments

---
+++ hardware/scripts/resize_designators.py
@@ -0,0 +1,32 @@
+import pcbnew
+
+# Load the current board
+board = pcbnew.GetBoard()
+
+# Set the desired font size
+new_size = pcbnew.FromMM(0.5)
+
+# Create a VECTOR2I object for the new font size
+new_size_vector = pcbnew.VECTOR2I(new_size, new_size)
+
+# Iterate through all the footprints on the board
+for module in board.GetFootprints():
+    reference = module.GetReference()
+
+    # Check if the footprint is a capacitor or resistor
+    if reference.startswith("C") or reference.startswith("R"):
+
+        # Get the reference text object
+        ref_text = module.Reference()
+
+        # Set the new font size
+        ref_text.SetTextSize(new_size_vector)
+
+        # Get the value text object
+        value_text = module.Value()
+
+        # Hide the value
+        value_text.SetVisible(False)
+
+# Refresh the display
+pcbnew.Refresh()
